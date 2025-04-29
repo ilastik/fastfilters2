6262856b48d7b538c9786271c4afa1efe7c6a6ac
Emil Melnikov <emilmelnikov@gmail.com>
2025-04-29 17:50:57 +0200

    Change how results are compared to fastfilters v1
    It is practically impossible to obtain results that are precisely equal
    to the old implementation without a loss in performance and loss in the
    implementation flexibility. Therefore some amount of numerical
    differences between implementations is allowed.
    
    Previously, we tried to compare maximum absolute differences between
    filter outputs, using the full uint8 dynamic range (mapped to float32).
    The reasoning was to be as close to the real-world usage as possible.
    This resulted in significant differences between implementations.
    
    However, the old implementation used much laxer tests in order to verify
    its outputs w.r.t. to the Vigra library. First, it used values in the
    [0; 1) range, where precision is much higher. Second, it used mean
    absolute difference (*not* max) for 3D multi-channel filters, where
    numerical differences are especially pronounced.
    
    Now, we are using the same strategy for the compatibility testing. The
    tests are a bit different: we test for more values of sigma (scale), but
    skip testing the structure tensor with the different derivative scale
    (because it is almost always derived from the main outer scale).
    
    Finally, the script `resultcompare.py` has been updated to match the
    test code.

--- scripts/resultcompare.py
+++ scripts/resultcompare.py
@@ -1,5 +1,7 @@
 """3-way comparison of results between Vigra, fastfilters, and fastfilters2."""
 
+import itertools
+
 import fastfilters
 import numpy
 import vigra.filters
@@ -8,7 +10,7 @@ from rich.table import Table
 
 import fastfilters2.compat
 
-RNG = numpy.random.default_rng(seed=42)
+RNG = numpy.random.Generator(numpy.random.PCG64DXSM(seed=42))
 FILTERS = (
     "gaussianSmoothing",
     "gaussianGradientMagnitude",
@@ -20,72 +22,64 @@ SHAPES = ((512, 512), (64, 64, 64))
 SCALES = (0.3, 0.7, 1.0, 1.6, 3.5, 5.0, 10.0)
 
 
-def max_abs_diff(a, b):
-    return numpy.max(numpy.abs(a - b))
-
-
-def mean_square_diff(a, b):
-    return numpy.mean(numpy.square(a - b))
+def abs_max_mean_diff(a, b):
+    diff = numpy.abs(a - b)
+    return numpy.max(diff), numpy.mean(diff)
 
 
 def main():
     table = Table()
+
     table.add_column("filter")
     table.add_column("shape")
     table.add_column("scale", justify="right")
-    table.add_column("max |vigra-ff1|", justify="right")
-    table.add_column("max |vigra-ff2|", justify="right")
-    table.add_column("max |ff1-ff2|", justify="right")
-    table.add_column("mean (vigra-ff1)\u00b2", justify="right")
-    table.add_column("mean (vigra-ff2)\u00b2", justify="right")
-    table.add_column("mean (ff1-ff2)\u00b2", justify="right")
+
+    table.add_column("max |ff2-ff1|", justify="right")
+    table.add_column("mean |ff2-ff1|", justify="right")
+
+    table.add_column("max |ff1-vigra|", justify="right")
+    table.add_column("mean |ff1-vigra|", justify="right")
+
+    table.add_column("max |ff2-vigra|", justify="right")
+    table.add_column("mean |ff2-vigra|", justify="right")
 
     with Live(table, auto_refresh=False) as live:
-        for name in FILTERS:
-            func_vigra = getattr(vigra.filters, name)
-            func_ff1 = getattr(fastfilters, name)
+        for name, shape, scale in itertools.product(FILTERS, SHAPES, SCALES):
             func_ff2 = getattr(fastfilters2.compat, name)
+            func_ff1 = getattr(fastfilters, name)
+            func_vigra = getattr(vigra.filters, name)
 
-            for shape in SHAPES:
-                data = RNG.integers(0, 256, size=shape).astype(numpy.float32)
+            data = RNG.random(shape, numpy.float32)
 
-                for scale in SCALES:
-                    if name == "structureTensorEigenvalues":
-                        args = (data, scale, 0.5 * scale)
-                    else:
-                        args = (data, scale)
+            if name == "structureTensorEigenvalues":
+                res_ff2 = func_ff2(data, scale, 0.5 * scale)
+                res_ff1 = func_ff1(data, scale, 0.5 * scale)
+                # In vigra, inner and outer scales are swapped.
+                res_vigra = func_vigra(data, 0.5 * scale, scale)
+            else:
+                res_ff2 = func_ff2(data, scale)
+                res_ff1 = func_ff1(data, scale)
+                res_vigra = func_vigra(data, scale)
 
-                    res_vigra = func_vigra(*args)
-                    res_ff1 = func_ff1(*args)
-                    res_ff2 = func_ff2(*args)
+            max_ff2_ff1, mean_ff2_ff1 = abs_max_mean_diff(res_ff2, res_ff1)
+            max_ff1_vigra, mean_ff1_vigra = abs_max_mean_diff(res_ff1, res_vigra)
+            max_ff2_vigra, mean_ff2_vigra = abs_max_mean_diff(res_ff2, res_vigra)
 
-                    mad_vigra_ff1 = max_abs_diff(res_vigra, res_ff1)
-                    mad_vigra_ff2 = max_abs_diff(res_vigra, res_ff2)
-                    mad_ff1_ff2 = max_abs_diff(res_ff1, res_ff2)
+            table.add_row(
+                name,
+                "\u00d7".join(map(str, shape)),
+                f"{scale:.1f}",
+                f"{max_ff2_ff1:.2e}",
+                f"{mean_ff2_ff1:.2e}",
+                f"{max_ff1_vigra:.2e}",
+                f"{mean_ff1_vigra:.2e}",
+                f"{max_ff2_vigra:.2e}",
+                f"{mean_ff2_vigra:.2e}",
+            )
 
-                    msd_vigra_ff1 = mean_square_diff(res_vigra, res_ff1)
-                    msd_vigra_ff2 = mean_square_diff(res_vigra, res_ff2)
-                    msd_ff1_ff2 = mean_square_diff(res_ff1, res_ff2)
+            live.refresh()
 
-                    style = ""
-                    if mad_vigra_ff1 < mad_ff1_ff2 or msd_vigra_ff1 < msd_ff1_ff2:
-                        style = "red"
-
-                    table.add_row(
-                        name,
-                        "\u00d7".join(map(str, shape)),
-                        f"{scale:.1f}",
-                        f"{mad_vigra_ff1:.1e}",
-                        f"{mad_vigra_ff2:.1e}",
-                        f"{mad_ff1_ff2:.1e}",
-                        f"{msd_vigra_ff1:.1e}",
-                        f"{msd_vigra_ff2:.1e}",
-                        f"{msd_ff1_ff2:.1e}",
-                        style=style,
-                    )
-                    live.refresh()
-
-            table.add_section()
+    table.add_section()
 
 
 if __name__ == "__main__":
--- tests/test_fastfilters2_compat.py
+++ tests/test_fastfilters2_compat.py
@@ -1,12 +1,13 @@
+from typing import Literal
+
 import numpy
 import pytest
 
-import fastfilters2
 import fastfilters2.compat
 
 fastfilters = pytest.importorskip("fastfilters")
 
-RNG = numpy.random.default_rng(seed=42)
+RNG = numpy.random.Generator(numpy.random.PCG64DXSM(seed=42))
 FILTERS = (
     "gaussianSmoothing",
     "gaussianGradientMagnitude",
@@ -17,62 +18,43 @@ FILTERS = (
 SHAPES = ((512, 512), (64, 64, 64))
 SCALES = (0.3, 0.7, 1.0, 1.6, 3.5, 5.0, 10.0)
 
-# For each particular filter, the top percentile and the number of ULPs to allow for the
-# compatibility test. Unfortunately, it seems impossible to achieve the bit-for-bit
-# identical results without the loss of performance.
-COMPAT_PARAMS = {
-    "gaussianGradientMagnitude": (0, 1),
-    "laplacianOfGaussian": (0, 2),
-    "hessianOfGaussianEigenvalues": (0.01, 20),
-    "structureTensorEigenvalues": (0.01, 60),
-}
 
-
-def assert_allclose_trimmed(a, b, *, tol, percentile=0):
-    """Assert that two arrays are close enough, but optionally allow for outliers.
-
-    The arrays are close if the given top percentile of the absolute differences between
-    the arrays' elements is no more than `tol`.
-
-    If the percentile is too small or 0, take the maximum absolute difference.
-    """
+def assert_similar(
+    actual, desired, *, mode: Literal["max", "mean"] = "max", atol: float = 1e-6
+):
     __tracebackhide__ = True
-    k = numpy.clip(round(0.01 * percentile * a.size), 1, a.size)
-    abs_diff = numpy.partition(numpy.abs(a - b), -k, axis=None)[-k]
-    if abs_diff > tol:
-        raise AssertionError(f"abs_diff = {abs_diff:.2e} > {tol:.2e} (k = {k})")
+
+    assert actual.shape == desired.shape
+    assert actual.dtype == desired.dtype
+    assert numpy.all(numpy.isfinite(actual))
+    assert numpy.all(numpy.isfinite(desired))
+
+    abs_diff = numpy.abs(actual - desired)
+
+    if mode == "max":
+        max_diff = numpy.max(abs_diff)
+        if max_diff > atol:
+            raise AssertionError(f"max(diff) = {max_diff:.2e} > {atol:.2e}")
+
+    elif mode == "mean":
+        mean_diff = numpy.mean(abs_diff)
+        if mean_diff > atol:
+            raise AssertionError(f"mean(diff) = {mean_diff:.2e} > {atol:.2e}")
+
+    else:
+        raise ValueError(f"Invalid mode: {mode}")
 
 
 @pytest.mark.parametrize("scale", SCALES)
 @pytest.mark.parametrize("shape", SHAPES)
 @pytest.mark.parametrize("name", FILTERS)
 def test_compat(name, shape, scale):
-    dtype = numpy.float32
-    vmin, vmax = 0, 255
-    data = RNG.integers(vmin, vmax, size=shape, endpoint=True).astype(dtype)
-
+    args = (RNG.random(shape, numpy.float32), scale)
     if name == "structureTensorEigenvalues":
-        args = (data, scale, 0.5 * scale)
-    else:
-        args = (data, scale)
+        args = (*args, 0.5 * scale)
 
-    func_ff2 = getattr(fastfilters2.compat, name)
-    func_ff1 = getattr(fastfilters, name)
+    actual = getattr(fastfilters2.compat, name)(*args)
+    desired = getattr(fastfilters, name)(*args)
 
-    actual = func_ff2(*args)
-    desired = func_ff1(*args)
-
-    percentile, nulp = COMPAT_PARAMS.get(name, (0, 0))
-    max_ulp = numpy.spacing(vmax, dtype=dtype)
-    tol = dtype(nulp) * max_ulp
-
-    assert_allclose_trimmed(actual, desired, tol=tol, percentile=percentile)
-
-
-def test_compat_small_data():
-    data = numpy.arange(24).reshape(2, 3, 4).astype(numpy.float32)
-    scale = 0.3
-    actual = fastfilters2.gaussian_smoothing(data, scale)
-    desired = fastfilters.gaussianSmoothing(data, scale)
-    tol = numpy.spacing(numpy.max(data), dtype=numpy.float32)
-    assert_allclose_trimmed(actual, desired, tol=tol)
+    mode = "mean" if len(shape) == 3 and name.endswith("Eigenvalues") else "max"
+    assert_similar(actual, desired, mode=mode)
