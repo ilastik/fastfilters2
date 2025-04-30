b18f40f1d5f4d3fb1035273cc6764b334104d38a
Emil Melnikov <emilmelnikov@gmail.com>
2025-04-30 12:58:44 +0200

    Add dynamic versioning via setuptools-scm

--- python/fastfilters2/__init__.py
+++ python/fastfilters2/__init__.py
@@ -6,8 +6,11 @@ from ._internal import (
     structure_tensor_eigenvalues,
     gaussian_derivative,
 )
+from ._version import __version__
+
 
 __all__ = (
+    "__version__",
     "gaussian_smoothing",
     "gaussian_gradient_magnitude",
     "hessian_of_gaussian_eigenvalues",
